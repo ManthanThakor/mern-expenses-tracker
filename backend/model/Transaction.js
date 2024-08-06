3e4a559d901560b0f4e87485e1af1033810b79b4
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-06 16:27:58 +0530

    mern expenses-tracker-project: 5

--- backend/model/Transaction.js
+++ backend/model/Transaction.js
@@ -1,1 +1,38 @@
-const m
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      default: "Uncategorized",
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
