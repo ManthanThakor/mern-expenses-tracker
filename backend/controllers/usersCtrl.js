241e4847f112663008421816a7c828b3c87bac16
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-07 10:04:03 +0530

    mern expenses-tracker-project: 10

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -1,1 +1,13 @@
+const asyncHandler = require("express-async-handler");
+
 //!  User Registration
+
+const usersController = {
+  //! Register
+  register: async (req, res) => {
+    res.json({ message: "Register" });
+  },
+
+  //! LOGIN
+  //! profile
+};
