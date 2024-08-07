d777526d8e1f19e73493bfad600944892c5f5987
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-07 13:30:51 +0530

    mern expenses-tracker-project: 13

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -1,13 +1,20 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../model/User");
 
-//!  User Registration
+//! User Registration
 
 const usersController = {
   //! Register
-  register: async (req, res) => {
+  register: asyncHandler(async (req, res) => {
+    // Registration logic goes here
     res.json({ message: "Register" });
-  },
+  }),
 
   //! LOGIN
+  // You can add the login handler here when needed
+
   //! profile
+  // You can add the profile handler here when needed
 };
+
+module.exports = usersController;
