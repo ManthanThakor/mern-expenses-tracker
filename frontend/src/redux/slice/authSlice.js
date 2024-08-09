a9fc6b4a181053c76f95f3c1135c381a3fa560b0
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-09 12:57:16 +0530

    mern expenses-tracker-project: frontend:15

--- frontend/src/redux/slice/authSlice.js
+++ frontend/src/redux/slice/authSlice.js
@@ -2,4 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 //!Initial State
 
-const
+const createSlice({
+  name:'auth',
+  initialState:{
+    user:null,
+  }
+})
+
+//1 Reducers
+reducers : {
+  loginAction: (state, action) => {
+    state.user = action.payload;
+  }
+}
+
+export default authSlice;
