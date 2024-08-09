5ed9c577d75f1bf3fcccfd4454c9289d405fa1b6
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-09 12:45:06 +0530

    mern expenses-tracker-project: frontend:11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import HeroSection from "./components/Home/HomePage";
+import LoginForm from "./components/Users/Login";
+import RegistrationForm from "./components/Users/Register";
 
 function App() {
   return (
